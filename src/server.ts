a95c7dba3c2f986f379cb2532f1048c3b68b6863
Ilona Mereuta <edoilo.3521@gmail.com>
2023-01-10 00:41:49 +0100

    config application with .env

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
+import "dotenv/config";
+
 import app from './app'
 
-const port = 3000;
+const port = process.env.PORT;
+
 app.listen(port, () => {
  console.log(`[server]: Server is running at localhost:${port}`)
 })
+
+
+
 
