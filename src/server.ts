d767af04c49bc663f30697d1f73495f405d196fc
Ilona Mereuta <edoilo.3521@gmail.com>
2023-01-09 21:48:03 +0100

    http server with express done

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { request, response } from "express"
+import "express-async-errors"
+
+const app = express();
+
+app.get("/", (request, response) =>{
+  response.send("Up and running");
+})
+
+
+const port = 3000;
+app.listen(port, () => {
+ console.log(`[server]: Server is running at localhost:${port}`)
+})
+
