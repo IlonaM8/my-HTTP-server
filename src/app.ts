370007172646f51fc69a42394d7da24e0226b846
Ilona Mereuta <edoilo.3521@gmail.com>
2023-01-10 00:30:44 +0100

    api route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
-import express, { request, response } from "express"
+import express from "express"
 import "express-async-errors"
 
 const app = express();
 
-app.get("/", (request, response) =>{
-  response.send("Up and running! And everything is automated");
+app.get("/planets", (request, response) =>{
+  response.json([
+    { name: "Pluto" },
+    { name: "Venus" },
+    { name: "Saturn" },
+    { name: "Jupiter" },
+]);
 })
 
 export default app;
